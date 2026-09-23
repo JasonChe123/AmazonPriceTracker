b34a85e0845a5801f8ef683816a40ac83cd181dc
JasonChe <kcche2001@yahoo.com.hk>
2026-09-23 22:35:06 +0100

    git ignore and run_tracker.sh minor changes

--- run_tracker.sh
+++ run_tracker.sh
@@ -6,8 +6,8 @@ cd /home/jasonche/Documents/Git-Repository/AmazonPriceTracker || exit
 # Activate virtual environment
 source .venv/bin/activate
 
-# Add a random delay between 0 and 1800 seconds (0 to 30 mins)
+# Add random delay between 0 and 1800 seconds (0 to 30 mins)
 python3 -c "import time, random; time.sleep(random.randint(0, 1800))"
 
-# Run tracker under Xvfb (Virtual Display for headful mode in background)
-xvfb-run -a python tracker.py
+# Run single-shot cron job under Xvfb and exit automatically
+xvfb-run -a python tracker.py --cron
